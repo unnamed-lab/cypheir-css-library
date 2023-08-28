f2f1f81fb0f44cc056ceb9398057eb5a3753ef45
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2023-08-28 02:26:43 +0100

    Ground work clean up

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import './assets/style.css'
 function App() {
   return (
     <>
-      <h1>Cypheir CSS Library</h1>
+      <h1>Cypheir CSS Library.</h1> 
+      <p>This page is under construction, kindly come back later. Thank you!</p>
     </>
   )
 }
